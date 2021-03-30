b340c038aa2e80ba56260a3fcdc80e0716aa4138
Jeff Heaton <jeff@jeffheaton.com>
2021-03-30 06:16:20 -0500

    Update to Wikipedia download

--- youtube/wikipedia/process/process_wikipedia.py
+++ youtube/wikipedia/process/process_wikipedia.py
@@ -1,10 +1,10 @@
 # Process_Wikipedia
 # Copyright 2021 by Jeff Heaton, released under the MIT License
 # https://github.com/jeffheaton
+import bs4
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import xml.etree.ElementTree as etree
-from bs4 import BeautifulSoup
 import multiprocessing as mp
 import posixpath
 import bz2
@@ -14,6 +14,9 @@ import time
 import os,glob
 import traceback
 import urllib
+import json
+import sys
+import hashlib
 
 WIKIPEDIA_URL = 'https://dumps.wikimedia.org/'
 WIKIPEDIA_LANG = 'enwiki'
@@ -21,6 +24,7 @@ WORKER_REPORT = 1000
 ENTIRE_TASK_REPORT = 100000
 QUEUE_SIZE = 50
 ENCODING = "utf-8"
+BUF_SIZE = 65536
 
 # Nicely formatted time string
 def hms_string(sec_elapsed):
@@ -58,26 +62,60 @@ def get_latest_wikidump(dumps):
         
     return max(lst)
 
+def sha1_file(path):
+    sha1 = hashlib.sha1()
+    with open(path, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha1.update(data)
+    return sha1.hexdigest()
+
 def wikidump_download(target_path, wiki_url=WIKIPEDIA_URL,wiki_lang=WIKIPEDIA_LANG, timestamp=None):
     if not timestamp:
         timestamp = get_latest_wikidump(get_wikidump_available(wiki_url,wiki_lang))
     
     dump_url = posixpath.join(wiki_url, wiki_lang, str(timestamp))
     print(dump_url)
-    dump_html = urllib.request.urlopen(dump_url).read()
-    soup_dump = BeautifulSoup(dump_html, 'html.parser')
-    files = soup_dump.find_all('li', {'class': 'file'})
     
-    for items in soup_dump.find_all('li', {'class': 'file'}):
-        for child in items.children:
-            if isinstance(child,bs4.element.Tag):
-                f = child.get('href', default=None)
-                if f and '-stub' not in f and '-current' in f and 'xml-' in f:
-                    url = urllib.parse.urljoin(wiki_url, f)
-                    a = urlparse(url)
-                    target_file = os.path.join(target_path,os.path.basename(a.path))
-                    print(url)
-                    urllib.request.urlretrieve(url, target_file)    
+    status_file = posixpath.join(dump_url, 'dumpstatus.json')
+    dump_json = urllib.request.urlopen(status_file).read()
+    dump_status = json.loads(dump_json)
+    dump_current = dump_status['jobs']['metacurrentdump']
+    job_status = dump_current['status']
+    
+    if job_status != 'done':
+        raise ValueError(f"Current article dump status: [job_status]")
+        
+    files = dump_current['files']
+    for file in files.keys():
+        meta = files[file]
+        source_url = urllib.parse.urljoin(wiki_url, meta['url'])
+        target_file = os.path.join(target_path,file)
+        if os.path.exists(target_file):
+            sha1_local = sha1_file(target_file)
+            if sha1_local != meta['sha1']:
+                print(f"Corrupt: {file}")
+                os.remove("file")
+                should_download = True
+            else:
+                print(f"Exists: {file}")
+                should_download = False
+        else:
+            print(f"Missing: {file}")
+            should_download = True
+            
+        if should_download:
+            try:
+                urllib.request.urlretrieve(source_url, target_file)
+                print(f"Downloaded: {file}")
+            except urllib.error.URLError as e:
+                try:
+                    os.remove(target_file)
+                finally:
+                    print(f"Download Error: {file}")
+
 
 class ExtractWikipediaFile:
     """This class is spun up once per worker process."""
